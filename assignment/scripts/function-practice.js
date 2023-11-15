9c81e5b9f9f3c39b6452653cce7c110062f010e0
Ashleigh Carter <ashbcarter@gmail.com>
2023-11-15 13:43:28 -0500

    hello, your name

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,10 +15,12 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  let title = 'Hello, Your Name!';
+  console.log(title);
+  return title;
 }
 // Remember to call the function to test
-
+console.log(helloName());
 
 // 3. Function to add two numbers together & return the result
 function addNumbers(firstNumber) {
